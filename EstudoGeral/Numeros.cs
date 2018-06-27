8ce64dfacca79e6006dd992a1eb76034e1075ddf
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2018-06-27 01:05:24 -0300

    Add project files.

---
+++ EstudoGeral/Numeros.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstudoGeral
+{
+    public class Numeros
+    {
+        public int[] ListaNumeros { get; set; }
+        
+        public Numeros()
+        {
+            ListaNumeros = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+        }
+        
+        public static bool FiltroPar(int numero)
+        {
+            return numero % 2 == 0;
+        }
+
+        public bool FiltroImpar(int numero)
+        {
+            return numero % 2 == 1;
+        }
+        
+        public void ImprimeListaNumeros(int[] lista, Filtro filtro)
+        {
+            foreach (var numero in lista)   
+            {
+                if(filtro(numero))
+                    Console.WriteLine(numero);
+            }
+            Console.WriteLine("\n----------\n");
+        }
+
+
+    }
+}
---
+++ EstudoGeral/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstudoGeral
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var entidade = new Numeros();
+
+            Filtro f = n => n % 3 == 0;
+
+            entidade.ImprimeListaNumeros(entidade.ListaNumeros, f);
+                
+            Console.ReadKey();
+        }
+    }
+}
